6050edad8515af33ffdd6c703b7e37d0f8d87063
Terranul <bfaraon920@gmail.com>
2026-08-18 14:49:24 -0700

    added ability to query and add email drafts

---
+++ Sources/EmailAnxietyBackend/Services/encoders.swift
@@ -0,0 +1,73 @@
+// gmail api is filled with things I don't need
+// this will just give me the important fields right when I encode
+
+import Foundation
+
+struct UserMessageLinks: Decodable {
+
+    var links: [String]
+
+    enum CodingKeys: CodingKey {
+        case messages
+    }
+
+    enum MessagesCodingKey: CodingKey {
+        case payload
+    }
+
+    enum PayloadCodingKey: CodingKey {
+        case body
+    }
+
+    enum BodyCodingKey: CodingKey {
+        case link
+    }
+
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let payload = try container.nestedContainer(keyedBy: MessagesCodingKey.self, forKey: .messages)
+        let body = try payload.nestedContainer(keyedBy: PayloadCodingKey.self, forKey: .payload)
+        var messages = try body.nestedUnkeyedContainer(forKey: .body)
+        self.links = []
+        while (!messages.isAtEnd) {
+            links.append(try messages.decode(String.self))
+        }
+    }
+}
+
+struct UserMessage: Decodable {
+    let mimeType: String?
+    let body: Body?
+    let parts: [UserMessage]?
+    
+    struct Body: Decodable {
+        let data: String?
+    }
+}
+
+struct Email {
+    let from: String
+    let to: String
+    let subject: String
+    let date: Date?
+    let body: String
+
+    init(from: String, to: String, subject: String, date: Date?, body: String) {
+        self.from = from
+        self.to = to
+        self.subject = subject
+        self.date = date
+        self.body = body
+    }
+
+    // return rfc 2822 encoding
+    func getRFCEncoding() -> String{
+        return """
+               From: \(self.from)
+               To: \(self.to)
+               Subject: \(self.subject)
+
+               \(self.body)
+               """    
+    }
+}
--- Sources/EmailAnxietyBackend/Services/gmailService.swift
+++ Sources/EmailAnxietyBackend/Services/gmailService.swift
@@ -4,7 +4,9 @@ enum RequestError: Error {
     case requestError
 }
 
-class gmailService {
+
+
+final class gmailService: Sendable {
 
     let accessCode: String
 
@@ -21,11 +23,55 @@ class gmailService {
         return urlRequest
     }
  
-    func getRecentMessages(scope: Int) async throws {
+    func getRecentMessageLinks(scope: Int) async throws -> UserMessageLinks {
         var (data, response) = try await URLSession.shared.data(for: try getURLRequest(path: ""))
         guard let httpResponse = response as? HTTPURLResponse else {
+            throw RequestError.requestError
+        }
+        var messages = try JSONDecoder().decode(UserMessageLinks.self, from: data)
+        messages.links.removeLast(messages.links.count - scope)
+        return messages
+    }
+
+    func getMessage(code: String) async throws -> UserMessage {
+        let request = try self.getURLRequest(path: "/gmail/v1/users/me/messages/\(code)?format=full")
+        var (data, response) = try await URLSession.shared.data(for: request)
+        var message = try JSONDecoder().decode(UserMessage.self, from: data)
+        return message
+    }
+
+    func getMessages(scope: Int) async throws -> [UserMessage] {
+        let links: [String] = await try getRecentMessageLinks(scope: scope).links
+        return await withThrowingTaskGroup(of: UserMessage.self) { body in
+        var messageResults = Array<UserMessage>()
+            for link in links {
+                body.addTask {
+                    return try await self.getMessage(code: link)
+                }
+                while !body.isEmpty {
+                    do {
+                        if let message = try await body.next() {
+                            messageResults.append(message)
+                        }
+                    } catch {
+                        body.cancelAll()
+                    }
+                }
+            }
+            return messageResults
+        }
+    }
+
+    func writeDraft(email: Email) async throws {
+        guard let body: Data = Data(base64Encoded: email.getRFCEncoding()) else {
+            debugPrint("issue with encoding email string")
             return
         }
+        var request = try self.getURLRequest(path: "https://gmail.googleapis.com/gmail/v1/users/me/drafts")
+        request.httpMethod = "POST"
+        request.httpBody = body
         
+
+
     }
 }
